b1cdeb44cef2551a18e01b4b78491db8e97ad775
jax <jax.maximillion.taylor@gmail.com>
2019-06-12 19:57:38 -0400

    update

--- Stage1 - Introducing Four in a Row/Stage1Step5 - Creating Class Files/js/Board.js
+++ Stage1 - Introducing Four in a Row/Stage1Step5 - Creating Class Files/js/Board.js
@@ -2,6 +2,21 @@ class Board {
     constructor(){
         this.rows = 6;
         this.columns = 7;
-        this.spaces = [];
+        this.spaces = this.createSpaces();
+    }
+
+    /** 
+     * Generates 2D array of spaces. 
+     * @return  {Array}     An array of space objects
+     */
+    createSpaces(){
+        const spaces = [];
+        for (let i = 0; i < this.rows; i++) {
+            for (let k = 0; k < this.columns; k++) {
+                const space = new Space(k, i);
+                spaces.push(space);
+            }
+        }
+        return spaces;
     }
 }
